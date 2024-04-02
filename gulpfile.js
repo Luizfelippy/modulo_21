34c14f214a26a4e77031c7261b13fe2da7c177b3
luizfelippy <luiz_felippy@hotmail.com>
2024-04-02 16:29:52 -0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 // importando pacotes instalados
 const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require('sass'));
+const imagemin = require ('gulp-imagemin');
 
-// criando uma função
+// criando funções
 function styles() {
     return gulp.src('./src/styles/*.scss')  //colentando os arquivos fontes
-    .pipe(sass({ outputStyle: 'compressed' })) //comprimindo os arquivos
-    .pipe(gulp.dest('./dist/css')); //criando e depositando arquivos comprimidos para pasta dist/css
+        .pipe(sass({ outputStyle: 'compressed' })) //comprimindo os arquivos
+        .pipe(gulp.dest('./dist/css')); //criando e depositando arquivos comprimidos para pasta dist/css
+    }
+
+function images() {
+    return gulp.src('./src/images/**/*')  //colentando os arquivos fontes
+        .pipe(imagemin()) //minificando os arquivos
+        .pipe(gulp.dest('./dist/images')); //criando e depositando arquivos minificados para pasta dist/images
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //função que atualiza mudanças feitas nos arquivos .scss
 }
