fc1d33ac6f05989cb58a511a5eb5ec7c554d7e53
luizfelippy <luiz_felippy@hotmail.com>
2024-04-20 14:22:48 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require('sass'));
 const imagemin = require ('gulp-imagemin');
+const uglify = require ('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 // criando funções
 function styles() {
@@ -16,8 +23,9 @@ function images() {
         .pipe(gulp.dest('./dist/images')); //criando e depositando arquivos minificados para pasta dist/images
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //função que atualiza mudanças feitas nos arquivos .scss
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
