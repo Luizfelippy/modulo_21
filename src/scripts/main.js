1ce36600451b88ba1e4649c1a86d04fdf3a5544c
luizfelippy <luiz_felippy@hotmail.com>
2024-05-25 14:13:30 -0300

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(button) {
+            const targetTab = button.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
+            hidesAllTabs();
+            tab.classList.add('movie__info--is-active');
+            removeActiveButton();
+            button.target.classList.add('movie__tabs__button--is-active');
+        })  
+    }
+
+    const scrollLinks = document.querySelectorAll('.button--is-scroll');
+
+    scrollLinks.forEach(function(scrollLink) {
+        scrollLink.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+})
+
+function removeActiveButton() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('movie__tabs__button--is-active');
+    }
+}
+
+
+function hidesAllTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('movie__info--is-active');
+    }
+}
